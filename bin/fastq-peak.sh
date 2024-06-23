869b210551d7916ec94c539378ef0375ca98b81e
kevinlibuit <Kevin@libuit.com>
2024-06-23 03:33:45 +0000

    remove utils source

--- bin/fastq-peak.sh
+++ bin/fastq-peak.sh
@@ -3,9 +3,6 @@
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$0")
 
-# Source the library file for reusable functions
-source "$SCRIPT_DIR/../lib/utils.sh"
-
 # Check if a FASTQ file is provided
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <fastq_file>"
@@ -22,11 +19,9 @@ fi
 
 echo "Processing FASTQ file: $FASTQ_FILE"
 
-# Count the number of reads in the FASTQ file #
 # Count the number of lines in the FASTQ file
 LINE_COUNT=$(wc -l < "$FASTQ_FILE")
-#Calculate the number of reads (4 lines per read)
-READ_COUNT=$((LINE_COUNT / 4))
-# Output the number of reads
-echo "Number of reads in $FASTQ_FILE: $READ_COUNT"
 
+# Calculate the number of reads (4 lines per read)
+READ_COUNT=$((LINE_COUNT / 4))
+echo "Number of reads in $FASTQ_FILE: $READ_COUNT"
