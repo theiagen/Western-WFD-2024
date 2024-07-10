dbd4f383915742770872afcec5066272fe2f455f
Holly Halstead <holly.halstead@doh.wa.gov>
2024-07-10 01:42:03 +0000

    adding gc-percentage calculation and reporting

--- bin/fastq-peek.sh
+++ bin/fastq-peek.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Holly Halstead 
+# Western-WFD-2024 Week 01 Exercise
+
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$0")
 
@@ -25,4 +28,14 @@ LINE_COUNT=$(wc -l < "$FASTQ_FILE")
 ## Calculate the number of reads (4 lines per read)
 READ_COUNT=$((LINE_COUNT / 4))
 
-echo "Number of reads in $FASTQ_FILE: $READ_COUNT"
+## Count GC in FASTQ file
+GC_COUNT=$(awk '(NR%4==2) {gsub(/[ATnNat]/,"");N+=length($0);}END{print N;}' "$FASTQ_FILE")
+
+## Count total number of bases in FASTQ file
+TOTAL_BASE_COUNT=$(awk 'NR%4==2 {sum += length($0)} END {print sum}' "$FASTQ_FILE")
+
+## Calculate GC percent in FASTQ file
+GC_PERCENT=$(awk '(NR%4==2) {N1+=length($0);gsub(/[AT]/,"");N2+=length($0);}END{print N2/N1;}' "$FASTQ_FILE")
+
+# Print results to terminal
+echo -e "Number of reads in $FASTQ_FILE: $READ_COUNT\nGC Percent in $FASTQ_FILE: $GC_PERCENT"
