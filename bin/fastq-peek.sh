ad30039987a6989093240ec0fa5687ad7c3b6104
Robert Dickerson <RE.Dickerson@outlook.com>
2024-07-15 17:52:23 +0000

    adds gc calc and reporting

--- bin/fastq-peek.sh
+++ bin/fastq-peek.sh
@@ -26,3 +26,13 @@ LINE_COUNT=$(wc -l < "$FASTQ_FILE")
 READ_COUNT=$((LINE_COUNT / 4))
 
 echo "Number of reads in $FASTQ_FILE: $READ_COUNT"
+
+# Calculate Percent GC
+## Count the number of G and C nucleotides
+GC_COUNT=$(grep -E '^[ATCGN]+$' "$FASTQ_FILE" | tr -cd 'GCgc' | wc -c)
+## Count the total number of nucleotides (A, T, C, G)
+TOTAL_BASE_COUNT=$(grep -E '^[ATCGN]+$' "$FASTQ_FILE" | tr -cd 'ATCGatcg' | wc -c)
+## Calculate the GC content as a percentage
+GC_CONTENT=$(awk "BEGIN {print ($GC_COUNT / $TOTAL_BASE_COUNT) * 100}")
+
+echo "GC content in $FASTQ_FILE: $GC_CONTENT%"
